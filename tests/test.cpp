5edad5bb6e823c62a66f7ae45e42995a7ee0e093
Birdswift <112816261+Birdswift@users.noreply.github.com>
2023-03-23 18:02:38 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,51 +5,44 @@
 #include <gtest/gtest.h>
 
 TEST(Account, Banking){
-//создаём тестовый объект
-	Account test(0,0);
-//проверяем GetBalance (и заодно конструктор)
-	ASSERT_EQ(test.GetBalance(), 0);
-//проверяем, что фича lock работает нормально
-	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
-	test.Lock();
-	ASSERT_NO_THROW(test.ChangeBalance(100));
-//проверяем, что изменение баланса работает
+	Account test(0,0); //constructor
+	ASSERT_EQ(test.GetBalance(), 0); 
+	// lock test
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error); // add balance
+	test.Lock(); //lock
+	ASSERT_NO_THROW(test.ChangeBalance(100)); //can we change?
+//change balance, check balance
 	ASSERT_EQ(test.GetBalance(), 100);
-//проверяем, что залочить уже залоченное нельзя
+//if it locked?
 	ASSERT_THROW(test.Lock(), std::runtime_error);
-//проверяем, что анлок работает
+//check unlock
 	test.Unlock();
 	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
-//радуемся жизни
 }
 
 TEST(Transaction, Banking){
-//создаём константы. base_fee для полноценного теста должен быть хотя бы 51.
 	const int base_A = 5000, base_B = 5000, base_fee = 100;
-//создаём тестовые объекты
-	Account Alice(0,base_A), Bob(1,base_B);
-	Transaction test_tran;
-//проверяем конструктор по умолчанию и сеттеры-геттеры
-	ASSERT_EQ(test_tran.fee(), 1);
-	test_tran.set_fee(base_fee);
-	ASSERT_EQ(test_tran.fee(), base_fee);
-//проверяем случаи когда транзакция не проходит
-	ASSERT_THROW(test_tran.Make(Alice, Alice, 1000), std::logic_error);
-	ASSERT_THROW(test_tran.Make(Alice, Bob, -50), std::invalid_argument);
-	ASSERT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
-	if (test_tran.fee()*2-1 >= 100)
-		ASSERT_EQ(test_tran.Make(Alice, Bob, test_tran.fee()*2-1), false);
-//проверяем, что всё правильно лочится
-	Alice.Lock();
-	ASSERT_THROW(test_tran.Make(Alice, Bob, 1000), std::runtime_error);
-	Alice.Unlock();
-//проверяем что если входные параметры правильные, то транзакция проходит православно
-	ASSERT_EQ(test_tran.Make(Alice, Bob, 1000), true);
-	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
-	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
-//проверяем что транзакция не проходит, если не хватает средств
-	ASSERT_EQ(test_tran.Make(Alice, Bob, 3900), false);
-	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
-	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
+	Account Man1(0,base_A), Man2(1,base_B);
+	Transaction tran;
+//constructors
+	ASSERT_EQ(tran.fee(), 1);
+	tran.set_fee(base_fee);
+	ASSERT_EQ(tran.fee(), base_fee);
+//when transaction doesnt work
+	ASSERT_THROW(tran.Make(Man1, Man1, 1000), std::logic_error);
+	ASSERT_THROW(tran.Make(Man1, Man2, -50), std::invalid_argument);
+	ASSERT_THROW(tran.Make(Man1, Man2, 50), std::logic_error);
+	if (tran.fee()*2-1 >= 100)
+		ASSERT_EQ(tran.Make(Man1, Man2, tran.fee()*2-1), false);
+//lock
+	Man1.Lock();
+	ASSERT_THROW(tran.Make(Man1, Man2, 1000), std::runtime_error);
+	Man1.Unlock();
+	ASSERT_EQ(tran.Make(Man1, Man2, 1000), true); //all good
+	ASSERT_EQ(Man2.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Man1.GetBalance(), base_A-1000-base_fee);
+	ASSERT_EQ(tran.Make(Man1, Man2, 3900), false);//not enough money
+	ASSERT_EQ(Man2.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Man1.GetBalance(), base_A-1000-base_fee);
 }
 
