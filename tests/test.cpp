5dbb8aed6c73d9af2ac1f7a08910d86353e3d60f
Birdswift <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>
2023-03-23 13:14:44 +0300

    11

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,78 +1,45 @@
-#include <Account.h>
-#include <Transaction.h>
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "Transaction.h"
+#include "Account.h"
 
-using ::testing::Return;
-
-class MockAccount : public Account {
-public:
-    MockAccount(int id, int balance) : Account(id, balance) {}
-
-    MOCK_METHOD(int, GetBalance, (), (const, override));
-    MOCK_METHOD(void, ChangeBalance, (int diff), (override));
-    MOCK_METHOD(void, Lock, (), (override));
-    MOCK_METHOD(void, Unlock, (), (override));
-};
-
-TEST(BankTest, Bank) {
-MockAccount t1(1, 220);
-MockAccount t2(2, 100);
-Transaction test;
-EXPECT_CALL(t1, GetBalance())
-.WillOnce(testing::Return(220))
-.WillOnce(testing::Return(69))
-.WillOnce(testing::Return(69))
-.WillOnce(testing::Return(69));
-EXPECT_CALL(t2, GetBalance())
-.WillOnce(testing::Return(250))
-.WillOnce(testing::Return(250));
-EXPECT_CALL(t1, ChangeBalance(-151))
-.WillOnce(testing::Invoke(nullptr));
-EXPECT_CALL(t2, ChangeBalance(testing::_))
-.Times(3);
-EXPECT_CALL(t1, Lock())
-.Times(5)
-.WillOnce(testing::Throw(std::runtime_error("at first lock the account")))
-.WillOnce(nullptr)
-.WillOnce(testing::Throw(std::runtime_error("already locked")))
-.WillOnce(nullptr)
-.WillOnce(nullptr);
-EXPECT_CALL(t2, Lock())
-.Times(2);
-EXPECT_CALL(t1, Unlock())
-.Times(3);
-EXPECT_CALL(t2, Unlock())
-.Times(2);
-
-EXPECT_THROW(test.Make(t1, t1, 50), std::logic_error);
-EXPECT_THROW(test.Make(t1, t2, 50), std::logic_error);
-EXPECT_THROW(test.Make(t1, t2, -50), std::invalid_argument);
-
-test.set_fee(200);
-EXPECT_EQ(test.Make(t1, t2, 150), 0);
-EXPECT_EQ(test.fee(), 200);
-test.set_fee(1);
-
-EXPECT_THROW(test.Make(t1, t2, 150), std::runtime_error);
-t1.Lock();
-EXPECT_THROW(test.Make(t1, t2, 150), std::runtime_error);
-t1.Unlock();
-
-bool res = test.Make(t1, t2, 150);
-EXPECT_EQ(res, 1);
-
-res = test.Make(t1, t2, 150);
-EXPECT_EQ(res, 0);
+//тест на работу аккаунта и баланса
+TEST(Transaction, account) {
+Account Acc(101, 100);
+EXPECT_EQ(Acc.GetBalance(), 100);
 }
 
-TEST(BankTest, Account) {
-Account t1(1, 500);
+//тест на условие закрытия аккаунта
+TEST(Transaction, change_balance) {
+    Account Acc(100, 100);
+    try {
+        Acc.ChangeBalance(100);
+    } catch (std::runtime_error& error){
+        EXPECT_EQ(error.what(), std::string("at first lock the account"));
+    }
+    Acc.Lock();
+    Acc.ChangeBalance(100);
+    EXPECT_EQ(Acc.GetBalance(), 200);
+}
 
-EXPECT_EQ(t1.GetBalance(), 500);
-EXPECT_THROW(t1.ChangeBalance(-100), std::runtime_error);
-t1.Lock();
-t1.ChangeBalance(-100);
-EXPECT_EQ(t1.GetBalance(), 400);
-t1.Unlock();
+
+
+//тест на перевод между аккаунтами и проверку баланса
+TEST(Transaction, transaction_sum) {
+        Account fAcc1(100, 110);
+        Account fAcc2(101, 101);
+        Account tAcc(200, 0);
+        Transaction transaction;
+        EXPECT_TRUE(transaction.Make(fAcc1, tAcc, 100));
+        EXPECT_FALSE(transaction.Make(fAcc2, tAcc, 105));
+}
+
+//тест на ошибку перевода на один и тот же аккаунт
+TEST(Transaction, transaction_id) {
+    Account Acc(100, 110);
+    Transaction transaction;
+    try {
+        EXPECT_FALSE(transaction.Make(Acc, Acc, 100));
+    } catch (std::logic_error& error){
+        EXPECT_EQ(error.what(), std::string("invalid action"));
+    }
 }
